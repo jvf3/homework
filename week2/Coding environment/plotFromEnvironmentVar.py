ca96239a8bdc7ddfcca1a2bc328c13b69c9b41ab
duangel <115361734+duangel@users.noreply.github.com>
2022-10-23 14:47:25 +0200

    Add files via upload

---
+++ week2/Coding environment/plotFromEnvironmentVar.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Oct 22 15:26:07 2022
+
+@author: angel
+"""
+
+import pandas as pd
+import os
+
+path = os.environ['RESEARCH_PATH']
+#RESEARCH_PATH  = r'C:\Users\angel\OneDrive\Desktop\MQF\Fall 2022\Digital Tools\duangel\homework'
+df = pd.read_csv(path + '\coding-environment-exercise.csv')
+df['date'] =  pd.to_datetime(df['date'], format='%Y-%m-%d')
+df = df.set_index('date')
+
+# Plot
+df.plot()
